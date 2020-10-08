1cc139beabead8d138f51c28f4b735048a587a7d
Ashay Aswale <asaswale@wpi.edu>
2020-10-08 15:59:12 -0400

    submission commit

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import numpy as np
 from linear_regression import *
 from sklearn.datasets import make_regression
 # Note: please don't add any new package, you should solve this problem using only the packages above.
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 '''
     Problem 2: Apply your Linear Regression
     In this problem, use your linear regression method implemented in problem 1 to do the prediction.
@@ -12,22 +12,27 @@ from sklearn.datasets import make_regression
     Note: please don't use any existing package for linear regression problem, use your own version.
 '''
 
-#--------------------------
+# --------------------------
 
 n_samples = 200
-X,y = make_regression(n_samples= n_samples, n_features=4, random_state=1)
+X, y = make_regression(n_samples=n_samples, n_features=4, random_state=1)
 y = np.asmatrix(y).T
 X = np.asmatrix(X)
 Xtrain, Ytrain, Xtest, Ytest = X[::2], y[::2], X[1::2], y[1::2]
 
 #########################################
-## INSERT YOUR CODE HERE
+# INSERT YOUR CODE HERE
+alpha = 1
+n_epoch = 10
+w = train(Xtrain, Ytrain, alpha, n_epoch)
+yhat = compute_yhat(Xtrain, w)
+train_loss = compute_L(yhat, Ytrain)
+test_loss = compute_L(yhat, Ytest)
+print("alpha:", alpha)
+print("n_epoch:", n_epoch)
 
-
-
-
-
+print("training loss:", train_loss)
+print("testing loss:", test_loss)
 
 
 #########################################
-
--- linear_regression.py
+++ linear_regression.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 # Note: please don't add any new package, you should solve this problem using only the packages above.
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 '''
     Problem 1: Linear Regression
     In this problem, you will implement the linear regression method based upon gradient descent.
@@ -10,10 +10,12 @@ import numpy as np
     Note: please don't use any existing package for linear regression problem, implement your own version.
 '''
 
-#--------------------------
-def compute_Phi(x,p):
+# --------------------------
+
+
+def compute_Phi(x, p):
     '''
-        Compute the feature matrix Phi of x. We will construct p polynoials, the p features of the data samples. 
+        Compute the feature matrix Phi of x. We will construct p polynoials, the p features of the data samples.
         The features of each sample, is x^0, x^1, x^2 ... x^(p-1)
         Input:
             x : a vector of samples in one dimensional space, a numpy vector of shape n by 1.
@@ -23,37 +25,46 @@ def compute_Phi(x,p):
             Phi: the design/feature matrix of x, a numpy matrix of shape (n by p).
     '''
     #########################################
-    ## INSERT YOUR CODE HERE
-
-
+    # INSERT YOUR CODE HERE
+    phi = []
+    for i in range(0, p):
+        phi.append(np.power(x, i))
+    # Phi = np.array(phi)
+    Phi = np.asarray(phi, dtype=float)
+    Phi = np.asmatrix(Phi, dtype=float)
+    Phi = np.matrix(Phi)
+    Phi = Phi.T
 
     #########################################
-    return Phi 
+    return Phi
 
-#--------------------------
+# --------------------------
+
+
 def compute_yhat(Phi, w):
     '''
         Compute the linear logit value (predicted value) of all data instances. z = <w, x>
         Here <w, x> represents the dot product of the two vectors.
         Input:
-            Phi: the feature matrix of all data instance, a float numpy matrix of shape n by p. 
-            w: the weights parameter of the linear model, a float numpy matrix of shape p by 1. 
+            Phi: the feature matrix of all data instance, a float numpy matrix of shape n by p.
+            w: the weights parameter of the linear model, a float numpy matrix of shape p by 1.
         Output:
             yhat: the logit value (predicted value) of all instances, a float numpy matrix of shape n by 1
         Hint: you could solve this problem using 1 line of code. Though using more lines of code is also okay.
     '''
     #########################################
-    ## INSERT YOUR CODE HERE
-
-
+    # INSERT YOUR CODE HERE
+    yhat = Phi * w
     #########################################
 
     return yhat
 
-    #--------------------------
-def compute_L(yhat,y):
+    # --------------------------
+
+
+def compute_L(yhat, y):
     '''
-        Compute the loss function: mean squared error. In this function, divide the original mean squared error by 2 for making gradient computation simple. Remember our loss function in the slides.  
+        Compute the loss function: mean squared error. In this function, divide the original mean squared error by 2 for making gradient computation simple. Remember our loss function in the slides.
         Input:
             yhat: the predicted sample labels, a numpy vector of shape n by 1.
             y:  the sample labels, a numpy vector of shape n by 1.
@@ -61,88 +72,83 @@ def compute_L(yhat,y):
             L: the loss value of linear regression, a float scalar.
     '''
     #########################################
-    ## INSERT YOUR CODE HERE
-
-
-
+    # INSERT YOUR CODE HERE
+    l = (yhat - y)
+    L = np.sum(np.power(l, 2))/len(y)/2
     #########################################
-    return L 
-
+    return L
 
 
 def compute_dL_dw(y, yhat, Phi):
     '''
-        Compute the gradients of the loss function L with respect to (w.r.t.) the weights w. 
+        Compute the gradients of the loss function L with respect to (w.r.t.) the weights w.
         Input:
-            Phi: the feature matrix of all data instances, a float numpy matrix of shape n by p. 
+            Phi: the feature matrix of all data instances, a float numpy matrix of shape n by p.
                Here p is the number of features/dimensions.
             y: the sample labels, a numpy vector of shape n by 1.
             yhat: the predicted sample labels, a numpy vector of shape n by 1.
         Output:
-            dL_dw: the gradients of the loss function L with respect to the weights w, a numpy float matrix of shape p by 1. 
+            dL_dw: the gradients of the loss function L with respect to the weights w, a numpy float matrix of shape p by 1.
 
     '''
     #########################################
-    ## INSERT YOUR CODE HERE
-
-
+    # INSERT YOUR CODE HERE
+    dL_dw = ((yhat - y).T * Phi)/(len(y))
 
     #########################################
-    return dL_dw
+    return dL_dw.T
 
 
-#--------------------------
-def update_w(w, dL_dw, alpha = 0.001):
+# --------------------------
+def update_w(w, dL_dw, alpha=0.001):
     '''
        Given the instances in the training data, update the weights w using gradient descent.
         Input:
             w: the current value of the weight vector, a numpy float matrix of shape p by 1.
-            dL_dw: the gradient of the loss function w.r.t. the weight vector, a numpy float matrix of shape p by 1. 
+            dL_dw: the gradient of the loss function w.r.t. the weight vector, a numpy float matrix of shape p by 1.
             alpha: the step-size parameter of gradient descent, a float scalar.
         Output:
             w: the updated weight vector, a numpy float matrix of shape p by 1.
         Hint: you could solve this problem using 1 line of code
     '''
-    
-    #########################################
-    ## INSERT YOUR CODE HERE
 
+    #########################################
+    # INSERT YOUR CODE HERE
+    w -= alpha*dL_dw
 
     #########################################
     return w
 
 
-#--------------------------
+# --------------------------
 def train(X, Y, alpha=0.001, n_epoch=100):
     '''
        Given a training dataset, train the linear regression model by iteratively updating the weights w using the gradient descent
         We repeat n_epoch passes over all the training instances.
         Input:
-            X: the feature matrix of training instances, a float numpy matrix of shape (n by p). Here n is the number of data instance in the training set, p is the number of features/dimensions.
-            Y: the labels of training instance, a numpy integer matrix of shape n by 1. 
+            X: the feature matrix of training instances, a float numpy matrix of shape (n by p).
+            Here n is the number of data instance in the training set, p is the number of features/dimensions.
+            Y: the labels of training instance, a numpy integer matrix of shape n by 1.
             alpha: the step-size parameter of gradient descent, a float scalar.
             n_epoch: the number of passes to go through the training set, an integer scalar.
         Output:
-            w: the weight vector trained on the training set, a numpy float matrix of shape p by 1. 
+            w: the weight vector trained on the training set, a numpy float matrix of shape p by 1.
     '''
 
     # initialize weights as 0
     w = np.mat(np.zeros(X.shape[1])).T
+    # print(w)
 
     for _ in range(n_epoch):
 
-    #########################################
-    ## INSERT YOUR CODE HERE
+        #########################################
+        # INSERT YOUR CODE HERE
+        # Back propagation: compute local gradients
+        yhat = compute_yhat(X, w)
+        dL_dw = compute_dL_dw(Y, yhat, X)
 
-    # Back propagation: compute local gradients 
-        
-
-        
-        
-    # update the parameters w
-        
+        # update the parameters w
+        w = update_w(w, dL_dw, alpha)
 
      #########################################
     return w
-
-
